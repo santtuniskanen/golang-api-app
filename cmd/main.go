82db37d97bf54d1ef94690ddd1b9bad145001d56
santtuniskanen <santtuniska02@gmail.com>
2024-01-09 18:01:35 +0200

    created initialisers

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.loadenv()
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run()
 }
